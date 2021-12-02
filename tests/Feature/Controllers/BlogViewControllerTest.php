e22fedc4219c7471e3c60e75f8e26ce2ecc8fc8d
mogata <ms46.dev@gmail.com>
2021-12-02 20:08:43 +0900

    ブログタイトルを一覧表示する

--- tests/Feature/Controllers/BlogViewControllerTest.php
+++ tests/Feature/Controllers/BlogViewControllerTest.php
@@ -2,19 +2,32 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BlogViewControllerTest extends TestCase
 {
+
+    use RefreshDatabase; // マイグレーションを実行する。お決まりで書く。
+
+
     /** @test index */
     public function ブログのTOPページを開ける()
     {
-        // $this->withoutExceptionHandling(); // エラー内容を表示できる（）
+         $this->withoutExceptionHandling(); // エラー内容を表示できる（）
 
-        $response = $this->get('/');
-        $response->assertOk(); // ステータスコードが200かどうか
+        // DBにブログが登録されている場合、トップページを開くと、ブログのタイトル一覧が表示されていることを表現する
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
+
+        $this->get('/')
+            ->assertOk() // ステータスコードが200かどうか
+            ->assertSee($blog1->title) // レスポンスにブログのタイトルが含まれているか
+            ->assertSee($blog2->title) // レスポンスにブログのタイトルが含まれているか
+            ->assertSee($blog3->title); // レスポンスにブログのタイトルが含まれているか
     }
 
 }
