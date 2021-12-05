9166703024d4331d8bb8ffee08fa9db5386f6126
mogata <ms46.dev@gmail.com>
2021-12-05 14:57:09 +0900

    ユーザー登録処理

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', [BlogViewController::class, 'index']);
 Route::get('/blogs/{blog}', [BlogViewController::class, 'show']);
 
 Route::get('signup', [SignUpController::class, 'index']);
+Route::post('signup', [SignUpController::class, 'store']);
 
--- tests/Feature/SignUpControllerTest.php
+++ tests/Feature/SignUpControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,6 +13,8 @@ use App\Http\Controllers;
  */
 class SignUpControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test index */
     public function ユーザー登録画面を開ける()
     {
@@ -19,10 +22,38 @@ class SignUpControllerTest extends TestCase
         // ** データの準備 **
         $this->get('signup')
             ->assertOk();
+    }
+
+    /** @test store */
+    public function ユーザー登録できる()
+    {
+        $this->withoutExceptionHandling();
+        // ** データの準備 **
 
         // ** 実行 **
 
         // ** 検証 **
+        // DBに保存
+        // ログインさせてからマイページにリダイレクト
 
+        $validData = [
+            'name' => '太郎',
+            'email' => 'aaa@bbb.net',
+            'password' => 'abcd1234',
+        ];
+
+        $this->post('signup', $validData)
+            ->assertOk();
+
+        // パスワードはハッシュ化されるので、unsetしておく
+        unset($validData['password']);
+
+        // DBにデータが保存されているか
+        $this->assertDatabaseHas('users', $validData);
+
+        // パスワードの検証（きちんとハッシュ化されて保存されているか）
+        $user = User::firstWhere($validData);
+        $this->assertNotNull($user); // userが登録されているか
+        $this->assertTrue(\Hash::check('abcd1234', $user->password)); // パスワードが正しいか
     }
 }
